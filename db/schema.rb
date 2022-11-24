95bf91c7b165ba357cccab6935086afc4b676c90
brahimdidi <brahimgb1team@gmail.com>
2022-11-24 15:45:15 +0000

    migrate the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_24_151905) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_153756) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "doctor_id", null: false
+    t.string "description"
+    t.date "date_of_appointment"
+    t.string "time_of_appointment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
+    t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
 
   create_table "doctors", force: :cascade do |t|
     t.string "name"
@@ -28,4 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_151905) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "appointments", "doctors"
+  add_foreign_key "appointments", "users"
 end
