baadf64d869e333c95fffde19c9a9d47e14405f7
kadenyi <treykadenyi@gmail.com>
2022-11-25 14:39:05 +0300

    Add appointment validations

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,10 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :doctor
+
+  # validations
+
+  validates :description, presence: true, length: { in: 3..150 }
+  validates :date_of_appointment, presence: true
+  validates :time_of_appointment, presence: true
 end
