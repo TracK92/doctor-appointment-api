cbd1115be175cb9171a01a151dbdb44a3e77b942
Aleksandra Ujvari <ujvari65@gmail.com>
2022-11-25 13:54:54 +0100

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :create, :login] do
-        get '/create', to: 'users#create'
-        post '/login', to: 'users#login'
+        post '/create', to: 'users#create'
+        get '/login', to: 'users#login'
         get '/doctors', to: 'doctors#index'
+        post '/appointments', to: 'appointments#create'
+        get '/appointments', to: 'appointments#index'
+        
       end
-      resources :appointments, only: [:index, :create, :show, :update, :destroy] do
-        resources :doctors, only: [:index, :create, :show, :update, :destroy]
+      resources :doctors, only: [:index, :create, :show, :update, :destroy] do
+        get '/appointments', to: 'appointments#index'
+        resources :appointments, only: [:index, :create, :show, :update, :destroy]
         get '/show_appointments', to: 'appointments#show'
         get '/show_doctors', to: 'doctors#show'
       end
