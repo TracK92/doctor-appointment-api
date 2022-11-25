fa45a74a279350e9530c957404d28306a4bdc188
kadenyi <treykadenyi@gmail.com>
2022-11-25 12:52:40 +0300

    Create methods for appointments

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,2 +1,46 @@
 class Api::V1::AppointmentsController < ApplicationController
+
+  
+  # GET /appointments
+  def index
+    appointments = Appointment.all
+    render json: appointments
+  end
+
+   # POST /appointments
+  def new
+    appointment = Appointment.new
+  end
+
+  def create
+    appointment = Appointment.new(appointment_params)
+    appointment.user_id = current_user.id
+    if appointment.save
+      render json: appointment
+    else
+      render json: {error: 'Error creating appointment'}
+    end
+  end
+
+  # GET /appointments/1
+  def show
+    render json: appointment
+  end
+
+  # PATCH/PUT /appointments/1
+  def update
+    appointment.update(appointment_params)
+    if appointment.save
+      render json: appointment
+    else
+      render json: {error: 'Error updating appointment'}
+    end
+  end
+
+  # DELETE /appointments/1
+  def destroy
+    appointment.destroy
+    render json: appointment
+  end
+
 end
