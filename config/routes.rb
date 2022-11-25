ae434a59e4983a1b70b4a1a4d0332f0f959a2d9d
brahimdidi <brahimgb1team@gmail.com>
2022-11-25 16:56:43 +0000

    add doctors and appointments

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  namespace :api , defaults: { format: :json } do
+  namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :create, :login, :show] do
-        # get '/doctors', to: 'doctors#index'
-        resources :doctors, only: [:index, :create, :show, :update, :destroy] do
-          resources :appointments , only: [:index, :create, :destroy]
-        end
+      resources :users, only: [:index, :create, :login] do
+        resources :appointments, only: [:index, :create, :show, :update, :destroy]
       end
+      get '/users/:id/doctors', to: 'doctors#show_user_doctors'   
+      get '/appointments', to: 'appointments#index'
+      get '/appointments/:id', to: 'appointments#show'
+
+      get '/doctors', to: 'doctors#index'   
+      get '/doctors/:id', to: 'doctors#show'
+      put '/doctors/:id', to: 'doctors#update'
+      delete '/doctors/:id', to: 'doctors#destroy'
+      post '/doctors/', to: 'doctors#create'
     end
   end
 end
