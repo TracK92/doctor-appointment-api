ec1dfff8ab61fa2248f58754b9a08c37a9de9cf6
brahimdidi <brahimgb1team@gmail.com>
2022-11-25 09:27:19 +0000

    create Users controller in V1 of the api app

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::UsersController < ApplicationController
+  def index
+    @users = User.all 
+    render json: {users : @users}
+  end
+
+  def create 
+    if User.find(name: user_params[:name])
+      render json: {error: 'Username already exists, please login or use another username to sign up'}, status: :unauthorized
+    else
+      @user = User.create(user_params)
+      render json: {message: 'User created correctly.'}, status: :ok
+    end
+  end
+
+  def login
+    if User.find(name: user_params[:name])
+      render json: {message: 'User created correctly.'}, status: :ok
+    else
+      render json: {error: 'Username does not exists, please sign up'}, status: :not_found
+    end
+  end
+end
