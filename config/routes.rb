830b87c4d09f780e0404bf438a78171a9827ce57
brahimdidi <brahimgb1team@gmail.com>
2022-11-25 14:52:35 +0000

    fix the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,11 @@
 Rails.application.routes.draw do
-  namespace :api do
+  namespace :api , defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: [:index, :create, :login] do
-        post '/create', to: 'users#create'
-        get '/login', to: 'users#login'
-        get '/doctors', to: 'doctors#index'
-        post '/appointments', to: 'appointments#create'
-        get '/appointments', to: 'appointments#index'
-        
-      end
-      resources :doctors, only: [:index, :create, :show, :update, :destroy] do
-        get '/appointments', to: 'appointments#index'
-        resources :appointments, only: [:index, :create, :show, :update, :destroy]
-        get '/show_appointments', to: 'appointments#show'
-        get '/show_doctors', to: 'doctors#show'
+      resources :users, only: [:index, :create, :login, :show] do
+        # get '/doctors', to: 'doctors#index'
+        resources :doctors, only: [:index, :create, :show, :update, :destroy] do
+          resources :appointments , only: [:index, :create, :destroy]
+        end
       end
     end
   end
