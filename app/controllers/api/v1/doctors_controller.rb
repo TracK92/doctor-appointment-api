d312cc95aa1181ffde9f387690da62415e5b54f6
Abby Nyakara <abbynyakara@gmail.com>
2022-11-25 13:02:27 +0300

    add doctors controller

---
+++ app/controllers/api/v1/doctors_controller.rb
@@ -0,0 +1,37 @@
+class Api::V1::DoctorsController < ApplicationController
+  def index
+    @doctors = Doctor.all
+    render json: @doctors
+  end
+
+  def show
+    @doctor = Doctor.find(params[:id])
+    render json: @doctor
+  end
+
+
+  def create
+    @doctor = Doctor.new(doctor_params)
+  end
+
+  def update
+    @doctor = Doctor.find(params[:id])
+    if @doctor.update(doctor_params)
+      render json: @doctor
+    else
+      render json: @doctor.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @doctor = Doctor.find(params[:id])
+    @doctor.destroy
+    render json: Doctor.all
+  end
+
+  private
+
+  def doctor_params
+    params.require(:doctor).permit(:name, :specialization, :photo)
+  end
+end
