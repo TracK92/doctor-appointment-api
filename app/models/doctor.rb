da99cb56cf10c73f7298541f2011e53c1a2e2ec6
kadenyi <treykadenyi@gmail.com>
2022-11-25 14:39:27 +0300

    Add doctor validations

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,3 +1,9 @@
 class Doctor < ApplicationRecord
-  has_many :appointments
+  has_many :appointments, foreign_key: :doctor_id, dependent: :destroy
+
+  # validations
+
+  validates :name, presence: true, length: { in: 3..50 }
+  validates :specialization, presence: true, length: { in: 3..50 }
+  validates :photo, presence: true
 end
