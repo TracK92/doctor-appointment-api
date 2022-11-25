087a360d83f877109dc3ca4e7d18c25c6b009ae9
brahimdidi <brahimgb1team@gmail.com>
2022-11-25 09:31:05 +0000

    fix linters

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,23 +1,24 @@
 class Api::V1::UsersController < ApplicationController
   def index
-    @users = User.all 
-    render json: {users : @users}
+    @users = User.all
+    render json: @users
   end
 
-  def create 
+  def create
     if User.find(name: user_params[:name])
-      render json: {error: 'Username already exists, please login or use another username to sign up'}, status: :unauthorized
+      render json: { error: 'Username already exists, please login or use another username to sign up' },
+             status: :unauthorized
     else
       @user = User.create(user_params)
-      render json: {message: 'User created correctly.'}, status: :ok
+      render json: { message: 'User created correctly.' }, status: :ok
     end
   end
 
   def login
     if User.find(name: user_params[:name])
-      render json: {message: 'User created correctly.'}, status: :ok
+      render json: { message: 'User created correctly.' }, status: :ok
     else
-      render json: {error: 'Username does not exists, please sign up'}, status: :not_found
+      render json: { error: 'Username does not exists, please sign up' }, status: :not_found
     end
   end
 end
