9814113eba4d547240ba0b8d88e596a4a04828e5
kadenyi <treykadenyi@gmail.com>
2022-11-25 15:19:43 +0300

    Validate date in the future and not the past

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,8 +3,8 @@ class Appointment < ApplicationRecord
   belongs_to :doctor
 
   # validations
-
+  # validate :date_of_appointment_cannot_be_in_the_past
   validates :description, presence: true, length: { in: 3..150 }
-  validates :date_of_appointment, presence: true
-  validates :time_of_appointment, presence: true
+  validates :date_of_appointment, presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: 'Date format should be YYYY-MM-DD' }, inclusion: { in: Date.today..Date.today + 5.years, message: 'Date should be in the future' }
+  validates :time_of_appointment, presence: true, format: { with: /\A\d{2}:\d{2}\z/, message: 'Time format should be HH:MM' }
 end
