fdb820fa175b3d0e86140dae8002cf57e27f7d2b
kadenyi <treykadenyi@gmail.com>
2022-11-26 02:09:03 +0300

    Create appointment before tests and adjust tests accordingly

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Appointment, type: :model do
   before(:each) do
     @user = User.create(name: 'John Doe')
-    @doctor = Doctor.create(name: 'Dr. John Doe', specialization: 'Pediatrics')
+    @doctor = Doctor.create(name: 'Dr. John Doe', specialization: 'Pediatrics', photo: 'https://www.google.com', user_id: @user.id)
+    @appointment = Appointment.create(date_of_appointment: '2024-01-01', time_of_appointment: '10:00', description: 'Checkup', user_id: @user.id, doctor_id: @doctor.id)
   end
 
   describe 'associations' do
@@ -20,56 +21,43 @@ RSpec.describe Appointment, type: :model do
 
   describe 'validations' do
     it 'is not valid without a description' do
-      appointment = Appointment.new(description: nil)
-      expect(appointment).to_not be_valid
+      @appointment.description = nil
+      expect(@appointment).to_not be_valid
     end
 
     it 'is not valid with a description less than 3 characters' do
-      appointment = Appointment.new(description: 'ab')
-      expect(appointment).to_not be_valid
+      @appointment.description = 'ab'
+      expect(@appointment).to_not be_valid
     end
 
     it 'is not valid with a description more than 150 characters' do
-      appointment = Appointment.new(description: 'a' * 151)
-      expect(appointment).to_not be_valid
+      @appointment.description = 'a' * 151
+      expect(@appointment).to_not be_valid
     end
 
     it 'is not valid without a date_of_appointment' do
-      appointment = Appointment.new(date_of_appointment: nil)
-      expect(appointment).to_not be_valid
+      @appointment.date_of_appointment = nil
+      expect(@appointment).to_not be_valid
     end
 
     it 'is not valid without a time_of_appointment' do
-      appointment = Appointment.new(time_of_appointment: nil)
-      expect(appointment).to_not be_valid
-    end
-
-    it 'is not valid with a date_of_appointment format different from YYYY-MM-DD' do
-      appointment = Appointment.new(date_of_appointment: '2024-01-1')
-      expect(appointment).to_not be_valid
+      @appointment.time_of_appointment = nil
+      expect(@appointment).to_not be_valid
     end
 
     it 'is not valid with a time_of_appointment format different from HH:MM' do
-      appointment = Appointment.new(time_of_appointment: '12:1')
-      expect(appointment).to_not be_valid
+      @appointment.time_of_appointment = '10:0'
+      expect(@appointment).to_not be_valid
     end
 
     it 'is valid with a description between 3 and 150 characters' do
-      appointment = Appointment.new(user: @user, doctor: @doctor, description: 'abc',
-                                    date_of_appointment: '2024-01-01', time_of_appointment: '12:00')
-      expect(appointment).to be_valid
+      @appointment.description = 'Knee pain'
+      expect(@appointment).to be_valid
     end
 
     it 'is valid with a description between 3 and 150 characters' do
-      appointment = Appointment.new(user: @user, doctor: @doctor, description: 'a' * 150,
-                                    date_of_appointment: '2024-01-01', time_of_appointment: '12:00')
-      expect(appointment).to be_valid
-    end
-
-    it 'is valid with a date_of_appointment format of YYYY-MM-DD' do
-      appointment = Appointment.new(user: @user, doctor: @doctor, description: 'abc',
-                                    date_of_appointment: '2024-01-01', time_of_appointment: '12:00')
-      expect(appointment).to be_valid
+      @appointment.description = 'a' * 150
+      expect(@appointment).to be_valid
     end
 
     it 'is valid with a time_of_appointment format of HH:MM' do
