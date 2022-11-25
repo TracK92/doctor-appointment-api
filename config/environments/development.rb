09e563db674d60bf1735c1312ad179426eaf5fbe
Aleksandra Ujvari <ujvari65@gmail.com>
2022-11-25 13:10:13 +0100

    Config initializers cors.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,8 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # Sets the format used in responses when errors occur in the development environment.
+  config.debug_exception_response_format = :api
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
