0b4b17adc943b6050df93450e4216b6b5ae2f446
kadenyi <treykadenyi@gmail.com>
2022-11-25 14:39:39 +0300

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-  has_many :appointments
+  has_many :appointments, foreign_key: :user_id, dependent: :destroy
+
+  # validations
+  validates :name, presence: true, length: { in: 3..50 }
 end
