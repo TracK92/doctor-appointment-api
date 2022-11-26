ba8d85a0f9a1e904aee797392609d43f7697d58d
Aleksandra Ujvari <ujvari65@gmail.com>
2022-11-26 19:06:25 +0100

    Add test for user login in controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,4 +10,10 @@ RSpec.describe 'Users', type: %w[request feature] do
   it 'creates a user' do
     expect(@user.name).to eq('Alex')
   end
+
+  it 'logs in a user' do
+    post '/api/v1/users/login',
+            params: { user: { name: 'Alex'} }
+    expect(response).to have_http_status(202)
+  end
 end
