262e3fa80d8970722eaa4a47006e14ba54db657e
brahimdidi <brahimgb1team@gmail.com>
2022-11-26 12:48:19 +0000

    fix linters

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -3,16 +3,14 @@ class Api::V1::AppointmentsController < ApplicationController
 
   # GET users/:id/appointments
   def index
+    # List the appointments for a specific user
     @appointments = Appointment.all.where(user_id: params[:user_id])
-    if @appointments.empty?
-      render json: { message: 'You have no appoinments yet' }
-    else
-      render json: @appointments, status: :ok
-    end
+    render json: @appointments, status: :ok
   end
 
   # GET /appointments
   def show_all
+    # Lists all the appointments
     @appointments = Appointment.all
     render json: @appointments, status: :ok
   end
