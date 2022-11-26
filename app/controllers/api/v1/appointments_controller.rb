b56fe3aa54ef186c8beb413a0c81ced7246f6e09
brahimdidi <brahimgb1team@gmail.com>
2022-11-26 12:28:08 +0000

    fix appointments index and  destroy methods , get appointments for a user

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -4,7 +4,11 @@ class Api::V1::AppointmentsController < ApplicationController
   # GET users/:id/appointments
   def index
     @appointments = Appointment.all.where(user_id: params[:user_id])
-    render json: @appointments, status: :ok
+    if @appointments.empty?
+      render json: {message: 'You have no appoinments yet'}
+    else
+      render json: @appointments, status: :ok
+    end
   end
 
   # GET /appointments
@@ -39,10 +43,14 @@ class Api::V1::AppointmentsController < ApplicationController
     end
   end
 
-  # DELETE /appointments/1
+  # DELETE users/1/appointments/1
   def destroy
-    @appointment.destroy
-    render json: @appointment, status: :ok
+    if params[:user_id].to_i == @appointment.user_id
+      @appointment.destroy
+      render json: { message: "Your appointment with doctor: #{@appointment.doctor.name} canceled!" }
+    else
+      render json: { error: 'Only the Client  can cancel this appointment' }, status: :forbidden
+    end
   end
 
   private
