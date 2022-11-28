7ad60c450d15c7fa60b93d7e1641e90b5f06b527
Tracey Kadenyi <treykadenyi@gmail.com>
2022-11-28 12:00:10 +0300

    Revert "Api"

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -3,14 +3,12 @@ class Api::V1::AppointmentsController < ApplicationController
 
   # GET users/:id/appointments
   def index
-    # List the appointments for a specific user
     @appointments = Appointment.all.where(user_id: params[:user_id])
     render json: @appointments, status: :ok
   end
 
   # GET /appointments
   def show_all
-    # Lists all the appointments
     @appointments = Appointment.all
     render json: @appointments, status: :ok
   end
@@ -41,14 +39,10 @@ class Api::V1::AppointmentsController < ApplicationController
     end
   end
 
-  # DELETE users/1/appointments/1
+  # DELETE /appointments/1
   def destroy
-    if params[:user_id].to_i == @appointment.user_id
-      @appointment.destroy
-      render json: { message: "Your appointment with doctor: #{@appointment.doctor.name} canceled!" }
-    else
-      render json: { error: 'Only the Client  can cancel this appointment' }, status: :forbidden
-    end
+    @appointment.destroy
+    render json: @appointment, status: :ok
   end
 
   private
