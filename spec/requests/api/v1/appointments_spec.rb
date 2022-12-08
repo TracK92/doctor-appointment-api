b2a4c46c8a0011270b65555569cf26d1fc93231f
kadenyi <treykadenyi@gmail.com>
2022-12-08 16:17:15 +0300

    Fix tests errors

--- spec/requests/api/v1/appointments_spec.rb
+++ spec/requests/api/v1/appointments_spec.rb
@@ -18,44 +18,6 @@ RSpec.describe 'api/v1/appointments', type: :request do
         end
       end
     end
-
-    post('create appointment') do
-      parameter({
-                  in: :header,
-                  type: :string,
-                  name: :Authorization,
-                  required: true,
-                  description: 'Client token'
-                })
-
-      response(200, 'successful') do
-        security [Authorization: []]
-        let(:Authorization) { "Authorization #{generate_token}" }
-
-        consumes 'application/json'
-
-        parameter name: :appointment, in: :body, schema: {
-          type: :object,
-          properties: {
-            date_of_appointment: { type: :date },
-            time_of_appointment: { type: :date },
-            description: { type: :string },
-            doctor_id: { type: :bigint },
-            user_id: { type: :integer }
-          },
-          required: %w[date_of_appointment time_of_appointment description doctor_id user_id]
-        }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
   end
 
   path '/api/v1/users/{user_id}/appointments/{id}' do
