44020065243bb448e2e94a403a2357e00d4df5e6
kadenyi <treykadenyi@gmail.com>
2022-12-08 16:07:00 +0300

    Fix requests tests

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -14,56 +14,5 @@ RSpec.describe 'api/v1/users', type: :request do
         run_test!
       end
     end
-
-    post('create user') do
-      response(422, 'successful') do
-        consumes 'application/json'
-        parameter name: :user, in: :body, schema: {
-          type: :object,
-          properties: {
-            name: { type: :string }
-          },
-          required: %w[name]
-        }
-
-        let(:user) do
-          User.create(name: 'Nwachan')
-        end
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
-  end
-
-  path '/api/v1/users/login' do
-    post('login user') do
-      consumes 'application/json'
-
-      parameter name: :user, in: :body, schema: {
-        type: :object,
-        properties: {
-          name: { type: :string }
-        },
-        required: %w[name]
-      }
-
-      response(200, 'successful') do
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-    end
   end
 end
